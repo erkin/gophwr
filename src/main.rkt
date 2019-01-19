6b1718c89a7019e6d6c2042fcb931844b5ac3ef8
erkin <erkinbatu@gmail.com>
2019-01-19 11:50:00 +0300

    Make version message exit

--- src/main.rkt
+++ src/main.rkt
@@ -2,19 +2,22 @@
 (require "config.rkt")
 (require "window.rkt")
 
+(define (display-version)
+  (displayln "Gophwr v0.0.1")
+  (displayln "Copyright (C) 2019 Erkin Batu Altunbaş")
+  (newline)
+  (display   "Each of this project's source code is subject ")
+  (displayln "to the terms of the Mozilla Public Licence v2.0")
+  (display   "If a copy of the MPL was not distributed with this ")
+  (displayln "file, you can obtain one at https://mozilla.org/MPL/2.0/")
+  (exit '()))
+
 (module+ main
   (command-line
    #:once-any
    (("--version" "-v")
-    "Print version and licensing information"
-    (begin
-      (displayln "Gophwr v0.0.1")
-      (displayln "Copyright (C) 2019 Erkin Batu Altunbaş")
-      (newline)
-      (display   "Each of this project's source code is subject ")
-      (displayln "to the terms of the Mozilla Public Licence v2.0")
-      (display   "If a copy of the MPL was not distributed with this ")
-      (displayln "file, you can obtain one at https://mozilla.org/MPL/2.0/"))))
+    "Show version and licence information"
+    (display-version)))
 
   (populate-menu-bar)
   (populate-options)
