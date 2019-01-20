e8ed0778d87bef4e020d71c2cc7abfda8629ad9b
erkin <erkinbatu@gmail.com>
2019-01-20 15:41:57 +0300

    whoops

--- src/const.rkt
+++ src/const.rkt
@@ -9,7 +9,7 @@
    (string-append *project-name* " " *project-version*)
    "Copyright (C) 2019 Erkin Batu Altunbaş"
    ""
-   "Each of this project's source code is subject "
+   "Each file of this project's source code is subject "
    "to the terms of the Mozilla Public Licence v2.0"
    "If a copy of the MPL was not distributed with this "
    "file, you can obtain one at https://mozilla.org/MPL/2.0/"))
