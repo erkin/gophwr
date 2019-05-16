30cfdaa247fe9489eeda1e5351e899b536c64ffe
erkin <erkinbatu@gmail.com>
2019-05-16 22:34:00 +0300

    No need for custom input-lines procedure

--- src/gopher.rkt
+++ src/gopher.rkt
@@ -1,37 +1,13 @@
-#lang racket/base
+#lang racket
 (provide get-page)
 
 (require "config.rkt")
 (require "const.rkt")
 (require "entry.rkt")
 
-(require racket/list)
-(require racket/string)
 (require racket/tcp)
 (require openssl)
 
-
-;; Just a nitpick.
-(define string->char
-  (compose car string->list))
-
-;;; \r\n is mandatory in Gopher
-(define (write-line str out)
-  (display (string-append str "\r\n") out))
-
-(define (read-lines in)
-  (define (read-loop in lines)
-    (let ((line (read-line in 'return-linefeed)))
-      (if (or (eof-object? line) (string=? line "."))
-          (begin
-           (close-input-port in)
-           lines)
-          (read-loop in (append lines (list line))))))
-  (read-loop in '()))
-
-;; To let the user know the error comes from the client, not the server.
-(define (error-string . strs)
-  (string-append* *project-name* " Error: " strs))
 
 ;; We're using a simple in-house parser because the official one is too
 ;; complicated for our simple but specific needs.
@@ -43,15 +19,66 @@
   ;; File type is mandatory if there's a path.
   (regexp
    (string-append
-    "^"                ; Regexp begins here
-      "([^/?#:]*)"     ; Domain can be anything, including IP addresses
-      "(:[0-9]+)?"     ; Port
-      "(/"             ; Path begins here
-        "([^/])"       ; Single character file type
-        "(/[^?#]*)*"   ; Rest of the path
-      ")?/?"           ; Path ends here
-    "$"                ; Regexp ends here
+    "^"              ; Regexp begins here
+      "([^/?#:]*)"   ; Domain can be anything, including IP addresses
+      "(:[0-9]+)?"   ; Port
+      "(/"           ; Path begins here
+        "([^/])"     ; Single character file type
+        "(/[^?#]*)*" ; Rest of the path
+      ")?"           ; Path ends here
+    "/?$"            ; Regexp ends here
     )))
+
+
+;; Just a nitpick.
+(define string->char
+  (compose car string->list))
+
+;;; \r\n is mandatory in Gopher
+(define (write-line str out)
+  (display (string-append str "\r\n") out))
+
+;; To let the user know the error comes from the client, not the server.
+(define (error-string . strs)
+  (string-append* *project-name* " error: " strs))
+
+(define (read-input in type)
+  (case type
+    ((#\1)
+     (generate-entries (port->lines in #:line-mode 'return-linefeed)))
+    ((#\0 #\7 #\m #\M #\p #\x)
+     (string-join (port->lines in #:line-mode 'return-linefeed) "\n"))
+    ((#\4 #\5 #\6 #\9 #\c #\d #\e #\s #\;)
+     ;; Save the file.
+     (port->bytes in))
+    ((#\g)
+     ;; Try to render the GIF.
+     (port->bytes in))
+    ((#\I)
+     ;; Determine image type and try to render it.
+     (port->bytes in))
+    (else
+     "File type not recognised.")))
+
+(define (connect-server host port type path)
+  (define-values (in out)
+    ;; Try to connect with SSL if it's enabled.
+    ;; Doesn't work reliably.
+    ;; TODO: Add back the fallback mechanism.
+    ((if *tls-enabled?*
+         ssl-connect
+         tcp-connect)
+     host port))
+  ;; Request the desired path.
+  (write-line path out)
+  (flush-output out)
+  (close-output-port out)
+  ;; Do something with the input port based on the type of the file
+  ;; we're expecting.
+  (let ((result (read-input in type)))
+    (if (or (non-empty-string? result) (bytes? result))
+        result
+        (error-string "The server returned nothing."))))
 
 (define (get-page address)
   (let ((urn (regexp-match magic-regexp address)))
@@ -76,38 +103,3 @@
            (or path "/")))
         ;; Return an error if the address is not valid.
         (error-string "Invalid path: " address))))
-
-(define (connect-server host port type path)
-  (define-values (in out)
-    ;; Try to connect with SSL if it's enabled.
-    ;; Doesn't work reliably.
-    ;; TODO: Add back the fallback mechanism.
-    ((if *tls-enabled?*
-         ssl-connect
-         tcp-connect)
-     host port))
-  ;; Request the desired path.
-  (write-line path out)
-  (flush-output out)
-  (close-output-port out)
-  ;; Read what the server returns.
-  (let ((result
-         (case type
-           ((#\1)
-            (generate-entries (read-lines in)))
-           ((#\0 #\m #\M #\p #\x)
-            (string-join (read-lines in) "\n"))
-           ((#\g)
-            ;; Try to render it.
-            (port->bytes in))
-           ((#\I)
-            ;; Determine file type and try to render it.
-            (port->bytes in))
-           ((#\4 #\5 #\6 #\9 #\c #\d #\e #\s #\;)
-            ;; Save the file.
-            (port->bytes in))
-           (else
-            "File type unrecognised."))))
-    (if (non-empty-string? result)
-        result
-        (error-string "The server returned nothing."))))
