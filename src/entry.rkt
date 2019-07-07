1f5131b04b959ee96723215665cdfdd23e259f5b
erkin <erkinbatu@gmail.com>
2019-07-07 20:06:27 +0300

    Restructure menu rendering code Warn the user for CSO/telnet links

--- src/entry.rkt
+++ src/entry.rkt
@@ -74,76 +74,83 @@
           (write-string content output-file))
       (close-output-port output-file))))
 
-(define (render-menu page content go-to)
-  (let* ((insert-text
-          (λ (style str)
-            (send* page
-              ;; Set the desired style before inserting the text
-              (change-style style)
-              (insert str))))
-         (insert-selector
-          (λ (style str clickback
-                    #:decorator (decorator #f)
-                    #:justified? (justified #f))
-            (when decorator
-              (insert-text d-usual (string-append "[" decorator "] ")))
-            (when justified
-              (send page insert (make-string 6 #\space)))
-            (let ((before (send page get-start-position))
-                  (_ (insert-text style str))
-                  (after (send page get-start-position)))
-              (send* page
-                (set-clickback before after clickback d-clicked)
-                (insert "\n"))))))
-    (for-each
-     (λ (line)
-       ;; Pesky end-of-file dot...
-       (unless (member line '("" "."))
-         (match-let (((list type text path domain port)
-                      (parse-selector line)))
-           (let* ((address (string-append domain ":" port "/" type path))
-                  (click (λ _ (go-to address))))
-             (case type
-               (("i")
-                (if (string=? path "TITLE")
-                    (insert-text d-title text)
-                    (insert-text d-usual (string-append
-                                          (make-string 6 #\space) text)))
-                (send page insert "\n"))
-               (("1")
-                (insert-selector d-menu text click #:justified? #t))
-               (("3")
-                (send page insert (make-string 6 #\space))
-                (insert-text d-error text))
-               (("0" "M" "c" "e" "m" "w" "x")
-                (insert-selector d-document text click #:decorator "txt"))
-               (("H" "h")
-                (if (and (> (string-length path) 4)
-                         (string=? "URL:" (substring path 0 4)))
-                    (insert-selector d-link text
-                                     (λ _ (send-url (substring path 4) #f))
-                                     #:decorator "url")
-                    (insert-selector d-document text click
-                                     #:decorator "htm")))
-               (("7")
-                (insert-selector
-                 d-menu text
-                 (λ _
-                   (when-let (query (get-text-from-user "Query" text))
-                             (go-to (string-append address "\t" query))))
-                 #:decorator "???"))
-               (("I" "g" "p" ":")
-                (insert-selector d-image text click #:decorator "img"))
-               (("4" "5" "6" "9" "P" "d" "s" ";" "<")
-                (insert-selector d-binary text click #:decorator "bin"))
-               (("+")
-                (insert-selector d-menu text click #:decorator "dup"))
-               (("2" "8" "T")
-                (insert-selector d-error text click #:decorator "tel"))
-               (else
-                (insert-text d-error
-                             (string-append "Unknown selector type: " type))
-                (insert-text d-usual (string-append " " text "\n"))))))))
-     content))
+(define (render-menu page lines go-to)
+  (define (insert-text style str)
+    (send* page
+      ;; Set the desired style before inserting the text
+      (change-style style)
+      (insert str)))
+  (define (insert-selector
+           style str clickback
+           #:decorator (decorator #f)
+           #:justified? (justified #f))
+    (when decorator
+      (insert-text d-usual (string-append "[" decorator "] ")))
+    (when justified
+      (send page insert (make-string 6 #\space)))
+    (let ((before (send page get-start-position))
+          (_ (insert-text style str))
+          (after (send page get-start-position)))
+      (send* page
+        (set-clickback before after clickback d-clicked)
+        (insert "\n"))))
+  (define (render-menu-item line)
+    ;; Pesky end-of-file dot...
+    (unless (member line '("" "."))
+      (match-let* (((list type text path domain port) (parse-selector line))
+                   (address (string-append domain ":" port "/" type path))
+                   (click (λ _ (go-to address))))
+        (case type
+          (("i")
+           (if (string=? path "TITLE")
+               (insert-text d-title text)
+               (insert-text d-usual (string-append
+                                     (make-string 6 #\space) text)))
+           (send page insert "\n"))
+          (("1")
+           (insert-selector d-menu text click #:justified? #t))
+          (("3")
+           (send page insert (make-string 6 #\space))
+           (insert-text d-error text))
+          (("0" "M" "c" "e" "m" "w" "x")
+           (insert-selector d-document text click #:decorator "txt"))
+          (("H" "h")
+           ;; Check for web links.
+           (if (and (> (string-length path) 4)
+                    (string=? "URL:" (substring path 0 4)))
+               (insert-selector d-link text
+                                ;; Send it to default web browser.
+                                (λ _ (send-url (substring path 4) #f))
+                                #:decorator "url")
+               (insert-selector d-document text click
+                                #:decorator "htm")))
+          (("7")
+           (insert-selector
+            d-menu text
+            (λ _
+              (when-let (query (get-text-from-user "Query" text))
+                        (go-to (string-append address "\t" query))))
+            #:decorator " ? "))
+          (("I" "g" "p" ":")
+           (insert-selector d-image text click #:decorator "img"))
+          (("4" "5" "6" "9" "P" "d" "s" ";" "<")
+           (insert-selector d-binary text click #:decorator "bin"))
+          (("+")
+           (insert-selector d-menu text click #:decorator "dup"))
+          (("2" "8" "T")
+           (insert-selector
+            d-error text
+            ;; Warn the user that we don't do CSO/telnet.
+            (λ _
+              (message-box "Unsupported type"
+                           "Session types (CSO/telnet) are not supported."
+                           #f '(ok caution)))
+            #:decorator "tel"))
+          (else
+           (insert-text d-error
+                        (string-append "Unknown selector type: " type))
+           (insert-text d-usual (string-append " " text "\n")))))))
+  ;; Read and render each line of the menu.
+  (for-each render-menu-item lines)
   ;; Reset style after rendering the page.
   (send page change-style d-usual))
